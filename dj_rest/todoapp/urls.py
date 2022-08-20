3ffa9fc2927b1a2dc15b2b5cba4985717b66058c
Gleb Sviridoff <gas53@bk.ru>
2022-08-20 09:06:32 +0300

    dz6 is over

--- dj_rest/todoapp/urls.py
+++ dj_rest/todoapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from todoapp.views import TODOModelViewSet, ProjectrModelViewSet
 from usersapp.views import UserModelViewSet
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView, TokenVerifyView)
 
 app_name = 'todoapp'
 
@@ -11,6 +12,8 @@ router.register('project', ProjectrModelViewSet)
 router.register('users', UserModelViewSet)
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(router.urls)),
     ]
